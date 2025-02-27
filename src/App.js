2e460ac460743943fdf58d3318d7a076bc106659
Geek-Tekina <tpbc1717@gmail.com>
2025-02-27 18:21:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}></View>
+  );
+}
